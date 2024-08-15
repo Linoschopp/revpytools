0c411cedb89223bc65aa3efa60b43cc02b77baa6
Linoschopp <143100072+Linoschopp@users.noreply.github.com>
2024-08-15 12:26:36 +0200

    Update tools.py

--- tools.py
+++ tools.py
@@ -14,10 +14,12 @@ def open_app(name):
   _time.sleep(0.25)
   _pag.hotkey("enter")
 
-def slash():
-  _pag.keyDown("shift")
-  _pag.hotkey("7")
-  _pag.keyUp("shift")
+def slash(n=1):
+  for _ in range(n):
+      _pag.keyDown("shift")
+      _pag.hotkey("7")
+      _pag.keyUp("shift")
+  return ""
 
 def open_url(url):
   open_app("Google Chrome")
@@ -25,7 +27,11 @@ def open_url(url):
   _pag.hotkey("ctrl", "l")
   _time.sleep(0.75)
   if not (url.startswith("http://") or  url.startswith("https://")):
-    url = "http://"+url
-  _pag.write(url)
+    url = f"http://"+url
+  url = url.split("/"):
+  for p in range(len(url)-1):
+      _pag.write(url[p])
+      slash()
+  _pag.write(url[-1])
   _time.sleep(0.25)
   _pag.hotkey("enter")
