2b990ebdc491fbaaa939fca3178465c8cfaa574e
Linoschopp <143100072+Linoschopp@users.noreply.github.com>
2024-08-15 11:53:01 +0200

    Create tools.py

---
+++ tools.py
@@ -0,0 +1,15 @@
+import pyautogui as _pag
+import time as _time
+
+def altf4(count=1, interval=0.2):
+  for x in range (count):
+    _pag.hotkey("alt", "f4")
+    if not x == count-1:
+      _time.sleep(interval)
+
+def open_app(name):
+  _pag.hotkey("gui")
+  _time.sleep(0.5)
+  _pag.write(name)
+  _time.sleep(0.25)
+  _pag.hotkey("enter")
