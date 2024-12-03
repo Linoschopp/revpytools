c0a5547d8204633f5d6f66795f3083ce4301d1be
Linoschopp <143100072+Linoschopp@users.noreply.github.com>
2024-12-03 10:00:13 +0100

    Update tools.py
    Begin tranfer from pyautogui to pynput

--- tools.py
+++ tools.py
@@ -1,5 +1,8 @@
 import pyautogui as _pag
+import pynput as _pynput
 import time as _time
+
+keyboard = pynput.keyboard.Controller()
 
 def altf4(count=1, interval=0.2):
   for x in range (count):
@@ -8,11 +11,11 @@ def altf4(count=1, interval=0.2):
       _time.sleep(interval)
 
 def open_app(name):
-  _pag.hotkey("win")
+  keyboard.tap(pynput.keyboard.Key.cmd)
   _time.sleep(0.75)
-  _pag.write(name)
+  keyboard.type(name)
   _time.sleep(0.25)
-  _pag.hotkey("enter")
+  keyboard.tap(pynput.keyboard.Key.enter)
 
 def slash(n=1):
   for _ in range(n):
@@ -24,7 +27,8 @@ def slash(n=1):
 def open_url(url, open_delay=2):
   open_app("Google Chrome")
   _time.sleep(open_delay)
-  _pag.hotkey("ctrl", "l")
+  with keyboard.pressed(pynput.keyboard.Key.ctrl):
+    keyboard.tap("l")
   _time.sleep(0.75)
   if not (url.startswith("http://") or  url.startswith("https://")):
     url = f"http://"+url
